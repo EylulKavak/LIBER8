3f8040ecf6644647d71aee73fc7fe6abb429f490
Eylul Kavak Oz <eylulkavakk@gmail.com>
2024-08-01 17:08:28 +0300

    Last Updates
    - Controller panel added.
    - Menu panels updated.

--- Assets/Scripts/MainMenu/PanelManager.cs
+++ Assets/Scripts/MainMenu/PanelManager.cs
@@ -4,6 +4,7 @@ public class PanelManager : MonoBehaviour
 {
     public GameObject mainMenuPanel;
     public GameObject settingsMenuPanel;
+    public GameObject controllerPanel;
 
     void Start()
     {
@@ -14,12 +15,21 @@ public class PanelManager : MonoBehaviour
     {
         mainMenuPanel.SetActive(true);
         settingsMenuPanel.SetActive(false);
+        controllerPanel.SetActive(false);
     }
 
     public void ShowSettingsMenu()
     {
         mainMenuPanel.SetActive(false);
         settingsMenuPanel.SetActive(true);
+        controllerPanel.SetActive(false);
+    }
+
+    public void ShowControllerPanel()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsMenuPanel.SetActive(false);
+        controllerPanel.SetActive(true);
     }
 
     public void QuitGame()
